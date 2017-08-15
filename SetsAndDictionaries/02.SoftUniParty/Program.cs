e9ab7328a3276744f759619737d6cf82823f0333
Rosen.Tsekov <r.tsekov@abv.bg>
2017-08-15 23:54:42 +0300

    Sets and Dictionaries

---
+++ SetsAndDictionaries/02.SoftUniParty/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _02.SoftUniParty
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public class Program
+	{
+		public static void Main()
+		{
+			string input = string.Empty;
+			SortedSet<string> vip = new SortedSet<string>();
+			SortedSet<string> guest = new SortedSet<string>();
+
+			while ((input = Console.ReadLine()) != "PARTY")
+			{
+				if (input.Take(1).All(char.IsDigit))
+				{
+					vip.Add(input);
+				}
+				else
+				{
+					guest.Add(input);
+				}
+			}
+			while ((input = Console.ReadLine()) != "END")
+			{
+				if (vip.Contains(input))
+				{
+					vip.Remove(input);
+				}
+				else
+				{
+					guest.Remove(input);
+				}
+			}
+			
+
+			var count = vip.Count() + guest.Count();
+			Console.WriteLine(count);
+			foreach (var vp in vip)
+			{
+				Console.WriteLine(vp);
+			}
+			foreach (var gs in guest)
+			{
+				Console.WriteLine(gs);
+			}
+		}
+	}
+}
---
+++ SetsAndDictionaries/03.CountSameValuesInArray/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _03.CountSameValuesInArray
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			SortedDictionary<double, double> countSum = new SortedDictionary<double, double>();
+
+			var input = Console.ReadLine().Split(new[] { ' ' },StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				if (countSum.ContainsKey(input[i]))
+				{
+					countSum[input[i]] += 1;
+				}
+				else
+				{
+					countSum.Add(input[i],1);
+
+				}
+			}
+
+			foreach (var cs in countSum)
+			{
+				Console.WriteLine($"{cs.Key} - {cs.Value} times");
+			}
+
+		}
+	}
+}
